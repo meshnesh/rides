ff3dfce96831cd6b9e720e70d6727ca241423d69
Christopher Kiilu <cndeti@gmail.com>
2016-05-26 12:12:36 +0000

    change bg on scroll

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
-$('body').scroll(function(){
-   console.log($(window).height());
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 50) {
+        $("header").css({
+            'background': '#fff',
+        });
+        $('nav a').addClass('scrolling');
+    } else {
+        $("header").css({
+            'background': '#000',
+        });
+        $("nav a").removeClass("scrolling");
+    }
 });
